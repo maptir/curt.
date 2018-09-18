1c120bac75b1a674ce84304dd685df462f64e745
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-09-18 21:16:26 +0700

    :fire: Remove unused route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,22 +2,12 @@ import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
-import DemoRedux from '../demo/DemoRedux'
-import DemoRouteComponent from '../demo/DemoRouteComponent'
 
 const routes = [
   {
     path: '/',
     exact: true,
     component: Home,
-  },
-  {
-    path: '/redux',
-    component: DemoRedux,
-  },
-  {
-    path: '/route-component/:number', // URL params
-    component: DemoRouteComponent,
   },
 ]
 
