53bd40c6a40be1c51d1eea02a58c032d2eb9639e
Tharit Pongsaneh <tharit.p@ku.th>
2018-09-28 18:37:30 +0700

    Add Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/common/Navbar'
+import Footer from './components/common/Footer'
 import RouterView from './router'
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <div>
           <Navbar />
           <RouterView />
+          <Footer />
         </div>
       </Router>
     )
