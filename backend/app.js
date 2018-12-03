9f34abfd44516ace5e1d6f4dd94f892dde4ec3ff
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-12-04 02:11:12 +0700

    Add error logging

--- backend/app.js
+++ backend/app.js
@@ -59,7 +59,7 @@ app.use('/orders', ordersRouter)
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   console.log('a fucking error occurs')
-  console.log(err.status)
+  console.log(err)
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
