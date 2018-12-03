824f0ae3e944bbee43fbd6468ec2d77be00654d6
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-12-04 06:02:40 +0700

    Add new payment microservice

---
+++ payment/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/orders/payment', async (req, res) => {
+  console.log(req.body)
+  const { data } = await axios.post(
+    'https://api.omise.co/charges',
+    {
+      description: 'Test payment',
+      amount: req.body.price + '00' || 99900,
+      currency: 'thb',
+      card: req.body.token_id
+    },
+    {
+      auth: {
+        username: 'skey_test_5e4w5otnueqvo3uvs0s'
+      }
+    }
+  )
+  return res.send(data)
+})
+
+app.listen(process.env.PORT || 4000)
+
+// http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
--- proxy/index.js
+++ proxy/index.js
@@ -1,44 +1,9 @@
-const express = require('express')
-const proxy = require('http-proxy-middleware')
-const axios = require('axios')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const app = express()
+var express = require('express')
+var proxy = require('http-proxy-middleware')
 
-app.use(bodyParser.json())
-app.use(cors())
+var app = express()
 
-app.post('/orders/payment', async (req, res) => {
-  console.log(req.body)
-  const { data } = await axios.post(
-    'https://api.omise.co/charges',
-    {
-      description: 'Test payment',
-      amount: req.body.price + '00' || 99900,
-      currency: 'thb',
-      card: req.body.token_id
-    },
-    {
-      auth: {
-        username: 'skey_test_5e4w5otnueqvo3uvs0s'
-      }
-    }
-  )
-  return res.send(data)
-})
-
-app.use(
-  '/',
-  (res, req, next) => {
-    console.log('proxying')
-    next()
-  },
-  proxy({ target: 'http://35.197.147.179', changeOrigin: true }),
-  (res, req, next) => {
-    console.log('proxyed')
-    next()
-  }
-)
+app.use('/', proxy({ target: 'http://35.197.147.179', changeOrigin: true }))
 
 app.listen(process.env.PORT || 3000)
 
