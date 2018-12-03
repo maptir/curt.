0aa459364cc1558dd0ce67573f1140caeb9ee094
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-12-04 04:23:03 +0700

    Add proxy

---
+++ proxy/index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var proxy = require('http-proxy-middleware')
+
+var app = express()
+
+app.use('/', proxy({ target: 'http://35.197.147.179', changeOrigin: true }))
+app.listen(process.env.PORT || 3000)
+
+// http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
