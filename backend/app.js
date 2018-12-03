8ea55e930f7b8937eb31dd3bf83d2f2d61525d84
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-12-04 03:06:09 +0700

    Remove quote

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ db.on('error', console.error.bind(console, 'connection error:'))
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
